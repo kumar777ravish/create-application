5ed5cc6e4f38efdab0e0478b7b3fe52ce7e24ac5
Ravish Kumar <ravishcistb@gmail.com>
2022-06-22 16:48:28 +0530

    learning C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Console.WriteLine(!(x > 3 && x < 10));
+            Console.ReadLine();
+        }
+    }
+}
